c9a157fcfad110a39afb87ec56b695786d92b9fc
John <johnny.lioup@gmail.com>
2024-02-11 16:37:13 +0000

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Logi13.github.io',
+    base: '/some-kind-of-blog',
+});
